a670b85aebc23a9b06aa68af2fa86c90a73daf40
MaikeraW <wagner.maiconhenrique@gmail.com>
2026-07-09 14:48:38 -0300

    developt project cards and carroussel for all screen sizes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,3 +13,131 @@ document.querySelectorAll(".navListItem a").forEach(link => {
         headNav.classList.remove("active");
     });
 });
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+    const cards = document.querySelectorAll(".projectCard");
+
+    const nextButton = document.querySelector(".next");
+    const prevButton = document.querySelector(".prev");
+
+
+    let currentIndex = 0;
+
+    let interval;
+
+
+    function showProject(index){
+
+
+        cards.forEach(card=>{
+            card.classList.remove("active");
+        });
+
+
+        cards[index].classList.add("active");
+
+
+    }
+
+
+
+    function nextProject(){
+
+        currentIndex++;
+
+        if(currentIndex >= cards.length){
+            currentIndex = 0;
+        }
+
+
+        showProject(currentIndex);
+
+    }
+
+
+
+    function prevProject(){
+
+        currentIndex--;
+
+
+        if(currentIndex < 0){
+
+            currentIndex = cards.length - 1;
+
+        }
+
+
+        showProject(currentIndex);
+
+    }
+
+
+
+    function startAutoSlide(){
+
+        interval = setInterval(()=>{
+
+            nextProject();
+
+        },5000);
+
+    }
+
+
+
+    function resetTimer(){
+
+        clearInterval(interval);
+
+        startAutoSlide();
+
+    }
+
+
+
+    nextButton.addEventListener("click",()=>{
+
+        nextProject();
+
+        resetTimer();
+
+    });
+
+
+
+    prevButton.addEventListener("click",()=>{
+
+        prevProject();
+
+        resetTimer();
+
+    });
+
+
+
+    const slider = document.querySelector(".ourProjectsSlider");
+
+
+    slider.addEventListener("mouseenter",()=>{
+
+        clearInterval(interval);
+
+    });
+
+
+
+    slider.addEventListener("mouseleave",()=>{
+
+        startAutoSlide();
+
+    });
+
+
+
+    startAutoSlide();
+
+
+});
